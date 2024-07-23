654845fb50fd79906e54e1483c4ef3349b9e97bd
sarthak-nference <166585429+sarthak-nference@users.noreply.github.com>
2024-07-23 14:36:12 +0530

    Update make.sh

--- scripts_build/make.sh
+++ scripts_build/make.sh
@@ -9,22 +9,23 @@ export VERSION_UND=`echo $VERSION | tr '.' '_'`
 #---
 # Build SnpEff
 #---
-
-cd /code/SnpEff/
-
+CURRENT_DIR=$(pwd)
+cd ${CURRENT_DIR}
+cd ..
+POINT_DIR=$(pwd)
 mvn clean compile assembly:single jar:jar
 
-cp target/SnpEff-$VERSION-jar-with-dependencies.jar /code/SnpEff/snpEff.jar
+cp ${POINT_DIR}/target/SnpEff-$VERSION-jar-with-dependencies.jar ${POINT_DIR}/snpEff.jar
 
 # Install JAR file in local Maven repo
 mvn install:install-file \
-	-Dfile=target/SnpEff-$VERSION.jar \
-	-DgroupId=org.snpeff \
-	-DartifactId=SnpEff \
-	-Dversion=$VERSION \
-	-Dpackaging=jar \
-	-DgeneratePom=true \
-	--quiet
+        -Dfile=target/SnpEff-$VERSION.jar \
+        -DgroupId=org.snpeff \
+        -DartifactId=SnpEff \
+        -Dversion=$VERSION \
+        -Dpackaging=jar \
+        -DgeneratePom=true \
+        --quiet
 
 cd - 
 
